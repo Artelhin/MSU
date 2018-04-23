f4438439dbad47ccf71a598206e012962c3b515d
artelhin <artelhin@gmail.com>
2018-04-23 12:59:28 +0300

    a bit of SyntAnalizer started

--- Model_Interpretator/src/SyntAn.cpp
+++ Model_Interpretator/src/SyntAn.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 class SyntAnalyser {
     Scanner lexParser;
+    //Runner runner; //NEEDS IMPLEMENTATION
+    Lex cur;
 
 public:
     SyntAnalyser(const char* path) {
@@ -16,8 +18,37 @@ public:
             cout << "Error occured on LexicalAnalyzer initializing: " << ex << endl;
             throw S_exep("SyntAnalyzer: Failed to initialize LexicalAnalyzer");
         }
-        cout << "SyntAnalyser initialized" << endl;
+        /*
+        try {
+            runner = Runner();
+        } catch(S_exep ex) {
+            cout << "Error occured on Runner initializing: " << ex << endl;
+            throw S_exep("SyntAnalyzer: Failed to initialize Runner");
+        }
+        */
+        cout << "SyntAnalyser initialized." << endl;
     }
 
+    bool checkAndRun() {
+        cur = lexParser.get_lex();
+        if (cur == LEX_PROGRAM) {
+            cur = lexParser.get_lex();
+            return program();
+        }
+        return false;
+    }
 
+    bool program() {
+        if (cur == LEX_BEGIN) {
+            cur = lexParser.get_lex();
+            innerBlock();
+            cur = lexParser.get_lex();
+            if (cur == LEX_END) return true;
+            return false;
+        }
+    }
+
+    void innerBlock() {
+
+    }
 }
